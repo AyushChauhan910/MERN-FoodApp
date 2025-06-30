50bd745be2d95a61aa25dcf4398bf23c3e23872f
AyushChauhan910 <talktoayushchauhan@gmail.com>
2025-06-30 22:17:41 +0530

    font added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Oswald', ...fontFamily.sans]
+      },
       keyframes: {
         fadeIn: {
           "0%": { opacity: 0, transform: "translateY(20px)" },
